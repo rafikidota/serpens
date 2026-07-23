23ffe8be88c402d7b2f3e8eeff04d11d301c0da1
rafiki <71207415+rafikidota@users.noreply.github.com>
2026-07-23 08:57:30 -0400

    chore: :wrench: migrate prettier configuration to prettier.config.mjs and update eslint default project settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,7 +28,12 @@ export default tseslint.config(
       sourceType: 'module',
       parserOptions: {
         projectService: {
-          allowDefaultProject: ['vitest.config.ts', 'tsdown.config.ts', 'test/*.ts'],
+          allowDefaultProject: [
+            'vitest.config.ts',
+            'tsdown.config.ts',
+            'prettier.config.mjs',
+            'test/*.ts',
+          ],
         },
         tsconfigRootDir: import.meta.dirname
       }
